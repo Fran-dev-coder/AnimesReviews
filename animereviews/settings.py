7d20abf81359a8af17b901f56e32875c99b846cb
Fran-zip <francis.gimber@gmail.com>
2025-04-24 16:27:59 -0300

    Added __pycache__ to .gitignore

--- animereviews/settings.py
+++ animereviews/settings.py
@@ -201,7 +201,7 @@ STATICFILES_DIRS = [
 ]
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
